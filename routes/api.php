b2a666f5789dbbca6cebfce14c7d9a52d75df9ce
Riani Aprianti <rianiaprianti@example.com>
2026-05-02 16:14:02 +0700

    Add middleware protection: public Read endpoints, admin-only Create/Update/Destroy

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,21 @@ Route::get('/katalog', function () {
     ]);
 });
 
-Route::apiResource('genres', GenreController::class);
-Route::apiResource('authors', AuthorController::class);
+// Public routes (Read only - no authentication required)
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{genre}', [GenreController::class, 'show']);
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{author}', [AuthorController::class, 'show']);
+
+// Admin protected routes (Create, Update, Destroy)
+Route::middleware('admin')->group(function () {
+    Route::post('/genres', [GenreController::class, 'store']);
+    Route::put('/genres/{genre}', [GenreController::class, 'update']);
+    Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
+
+    Route::post('/authors', [AuthorController::class, 'store']);
+    Route::put('/authors/{author}', [AuthorController::class, 'update']);
+    Route::delete('/authors/{author}', [AuthorController::class, 'destroy']);
+});
+
 Route::get('/books', [BookController::class, 'index']);
